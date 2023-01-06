0283dcb5e6ab74e2628135558f24fd9c6973aab8
Andrey Makarov <viva.la.akam@gmail.com>
2023-01-06 14:49:17 +0400

    feat: add events to provider-base

--- src/provider.ts
+++ src/provider.ts
@@ -1,11 +1,13 @@
 import {ProviderBaseOptions, WalletInterface} from './types';
 import converter from 'bech32-converting';
+import EventEmitter from 'events';
 
-export class Provider<T extends object> {
+export class Provider<T extends object> extends EventEmitter {
   protected _wallet: WalletInterface;
   protected _options: T & ProviderBaseOptions;
 
   constructor(wallet: WalletInterface, options: T & ProviderBaseOptions) {
+    super()
     this._wallet = wallet;
     this._options = options;
   }

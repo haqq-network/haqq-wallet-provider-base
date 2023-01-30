8a5d1c51681f0ea150990ccc2df9a8f017869abc
Andrey Makarov <viva.la.akam@gmail.com>
2023-01-30 14:52:48 +0400

    fix: cleanup

--- src/provider.ts
+++ src/provider.ts
@@ -1,5 +1,6 @@
 import {ProviderBaseOptions} from './types';
 import EventEmitter from 'events';
+import converter from 'bech32-converting';
 
 export class Provider<T extends object> extends EventEmitter {
   protected _options: T & ProviderBaseOptions;
@@ -7,6 +8,15 @@ export class Provider<T extends object> extends EventEmitter {
   constructor(options: T & ProviderBaseOptions) {
     super()
     this._options = options;
+  }
+
+  getEthAddress(_hdPath: string) {
+    return Promise.resolve('')
+  }
+
+  async getCosmosAddress(hdPath: string) {
+    const address = await this.getEthAddress(hdPath)
+    return converter(this._options.cosmosPrefix).toBech32(address);
   }
 
   abort() {
